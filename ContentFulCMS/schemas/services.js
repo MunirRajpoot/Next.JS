7cbe48a03014fc2c06590a051bb0bdc5d1b1972a
MunirRajpoot <munirrajpoot1012@gmail.com>
2024-05-29 16:28:10 +0500

    Rest_Api_Build

---
+++ ContentFulCMS/schemas/services.js
@@ -0,0 +1,15 @@
+
+
+export default services = {
+
+    name: "services",
+    title: "",
+    type: "document",
+    fields: [
+        {
+            title: "Greeting",
+            name: "greeting",
+            type: "string"
+        }
+    ]
+}
